04026ef7424cf50a0ca616c2ef8de18bfd39c560
Mike Goodfellow <mdgoodfellow@gmail.com>
2017-11-08 16:22:01 +0000

    FIXED: Use atttribute on the pages as well

--- src/MobileTabTest.Core/Pages/Tab1.xaml.cs
+++ src/MobileTabTest.Core/Pages/Tab1.xaml.cs
@@ -1,5 +1,8 @@
-﻿namespace MobileTabTest.Core.Pages
+﻿using MvvmCross.Forms.Views.Attributes;
+
+namespace MobileTabTest.Core.Pages
 {
+    [MvxTabbedPagePresentation(TabbedPosition.Tab)]
     public partial class Tab1
     {
         public Tab1()
--- src/MobileTabTest.Core/Pages/Tab2.xaml.cs
+++ src/MobileTabTest.Core/Pages/Tab2.xaml.cs
@@ -1,5 +1,8 @@
-﻿namespace MobileTabTest.Core.Pages
+﻿using MvvmCross.Forms.Views.Attributes;
+
+namespace MobileTabTest.Core.Pages
 {
+    [MvxTabbedPagePresentation(TabbedPosition.Tab)]
     public partial class Tab2
     {
         public Tab2()
