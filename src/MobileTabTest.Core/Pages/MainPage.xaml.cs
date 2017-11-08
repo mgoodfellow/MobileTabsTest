3bec053c2fd00d95f681aa3d1c36be05d85f67b2
Mike Goodfellow <mdgoodfellow@gmail.com>
2017-11-08 16:20:26 +0000

    CHANGED: Move attribute, and make sure models load from code behind

--- src/MobileTabTest.Core/Pages/MainPage.xaml.cs
+++ src/MobileTabTest.Core/Pages/MainPage.xaml.cs
@@ -1,10 +1,32 @@
+using MvvmCross.Forms.Views.Attributes;
+
 namespace MobileTabTest.Core.Core.Pages
 {
+    [MvxTabbedPagePresentation(TabbedPosition.Root)]
     public partial class MainPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private bool _firstTime = true;
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (_firstTime)
+            {
+                //ViewModel.ShowInitialViewModelsCommand.Execute();
+                ViewModel.ShowInitialViewModelsCommand.ExecuteAsync(null);
+                _firstTime = false;
+            }
+        }
+
+        protected override void OnViewModelSet()
+        {
+            base.OnViewModelSet();
+
         }
     }
 }
