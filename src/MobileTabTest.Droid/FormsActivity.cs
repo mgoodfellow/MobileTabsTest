b74161c8b87a1816160ebd08b67bb281e49a58a6
Mike Goodfellow <mike@audiu.net>
2017-11-25 14:25:15 +0000

    FIXED: This is now working

--- src/MobileTabTest.Droid/FormsActivity.cs
+++ src/MobileTabTest.Droid/FormsActivity.cs
@@ -7,8 +7,12 @@ using MvvmCross.Platform;
 
 namespace MobileTabTest.Droid
 {
-    [Activity(Label = "MobileTabTest.Droid", Icon = "@drawable/icon", Theme = "@style/AppTheme",
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(
+        Label = "MobileTabTest.Droid", 
+        Icon = "@drawable/icon", 
+        Theme = "@style/AppTheme",
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+        LaunchMode = LaunchMode.SingleTask)]
     public class FormsActivity : MvxFormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
@@ -17,6 +21,11 @@ namespace MobileTabTest.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+        }
+
+        public override void OnBackPressed()
+        {
+            MoveTaskToBack(false);
         }
     }
 }
